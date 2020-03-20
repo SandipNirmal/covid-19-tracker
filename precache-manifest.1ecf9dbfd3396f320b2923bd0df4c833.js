2c55fe7f90cbc9d1b0ac5c05ee9a0cf21f936107
Sandip Nirmal <sandipnirmal@icloud.com>
2020-03-20 11:27:44 +0530

    Updates

---
+++ precache-manifest.1ecf9dbfd3396f320b2923bd0df4c833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9718ac45f7cd59300063e2c43464830",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "9efb8f4a0ff00d3d00fc",
+    "url": "/covid-19-tracker/static/css/main.549e221d.chunk.css"
+  },
+  {
+    "revision": "76a22e4052ca0e3ffe9c",
+    "url": "/covid-19-tracker/static/js/2.31d85802.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-tracker/static/js/2.31d85802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9efb8f4a0ff00d3d00fc",
+    "url": "/covid-19-tracker/static/js/main.60fa4060.chunk.js"
+  },
+  {
+    "revision": "c7ee893204412e748ec1",
+    "url": "/covid-19-tracker/static/js/runtime-main.72171ba5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30bd35013ff2fe3fb0912ed293026d4c.js"
+  "/covid-19-tracker/precache-manifest.1ecf9dbfd3396f320b2923bd0df4c833.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-19-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
