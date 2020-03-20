fae2d41d965b4f368ee31a9f3429643136e7d511
Sandip Nirmal <sandipnirmal@icloud.com>
2020-03-20 11:26:25 +0530

    Updates

---
+++ precache-manifest.30bd35013ff2fe3fb0912ed293026d4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e62b3eca51bfdd8d45d0ee3c255e521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d87be183158b7287a7b",
+    "url": "/static/css/main.549e221d.chunk.css"
+  },
+  {
+    "revision": "76a22e4052ca0e3ffe9c",
+    "url": "/static/js/2.31d85802.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31d85802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d87be183158b7287a7b",
+    "url": "/static/js/main.31c98f25.chunk.js"
+  },
+  {
+    "revision": "0b2d92d4cbbadab22fc1",
+    "url": "/static/js/runtime-main.c63f6491.js"
+  }
+]);
