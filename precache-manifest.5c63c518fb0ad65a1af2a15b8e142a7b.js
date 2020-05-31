850166a8f6d4325b2fe4ce353a1bf9c263032ab0
SandipNirmal <nirmal.sandip13389@gmail.com>
2020-05-31 12:58:31 +0000

    Deploying to gh-pages from  @ b3a0349d1524e46de1f4333ed24f35a61fd3c6cc 🚀

---
+++ precache-manifest.5c63c518fb0ad65a1af2a15b8e142a7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d07fbfe64aa51f7e541593a134f104",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "feddbf8ec02541b8ed7b",
+    "url": "/covid-19-tracker/static/css/main.e58c084d.chunk.css"
+  },
+  {
+    "revision": "8ee512d13213c6f01f20",
+    "url": "/covid-19-tracker/static/js/2.58a6ba4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-tracker/static/js/2.58a6ba4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feddbf8ec02541b8ed7b",
+    "url": "/covid-19-tracker/static/js/main.bb267445.chunk.js"
+  },
+  {
+    "revision": "c7ee893204412e748ec1",
+    "url": "/covid-19-tracker/static/js/runtime-main.72171ba5.js"
+  }
+]);
